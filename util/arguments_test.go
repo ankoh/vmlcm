2654f467a932bff1c3fea8efc7fd7623e99ff055
Andre Kohn <andre@kohn.io>
2015-10-24 17:14:51 +0200

    Using better test output

--- util/arguments_test.go
+++ util/arguments_test.go
@@ -12,48 +12,41 @@ func TestArguments(t *testing.T) {
 	Convey("Given a clean command line environment", t, func() {
     flag.CommandLine = flag.NewFlagSet("vmlcm", flag.ContinueOnError)
 
-		Convey("When the flag value is separated with whitespace", func() {
+		Convey("whitespace flags must be parsed correctly", func() {
       os.Args = []string{
         "vmlcm",
         "-f", "./agents.json",
         "verify"}
 
-      Convey("ParseArguments must return correct values", func() {
-        args, err := util.ParseArguments()
-        So(err, ShouldBeNil)
-        So(args, ShouldNotBeNil)
-        So(args.ConfigPath, ShouldNotBeNil)
-        So(*args.ConfigPath, ShouldEqual, "./agents.json")
-        So(args.Command, ShouldEqual, util.VerifyCommand)
-      })
+      args, err := util.ParseArguments()
+      So(err, ShouldBeNil)
+      So(args, ShouldNotBeNil)
+      So(args.ConfigPath, ShouldNotBeNil)
+      So(*args.ConfigPath, ShouldEqual, "./agents.json")
+      So(args.Command, ShouldEqual, util.VerifyCommand)
 		})
 
-    Convey("When the flag value is separated with an equal sign", func() {
+    Convey("non-whitespace flags must be parsed correctly", func() {
       os.Args = []string{
         "vmlcm",
         "-f=./agents.json",
         "up", "3"}
 
-      Convey("ParseArguments must return correct values", func() {
-        args, err := util.ParseArguments()
-
-        So(err, ShouldBeNil)
-        So(args, ShouldNotBeNil)
-        So(args.ConfigPath, ShouldNotBeNil)
-        So(*args.ConfigPath, ShouldEqual, "./agents.json")
-        So(args.Command, ShouldEqual, util.UpCommand)
-        So(args.CommandParameter, ShouldEqual, 3)
-      })
+      args, err := util.ParseArguments()
+      So(err, ShouldBeNil)
+      So(args, ShouldNotBeNil)
+      So(args.ConfigPath, ShouldNotBeNil)
+      So(*args.ConfigPath, ShouldEqual, "./agents.json")
+      So(args.Command, ShouldEqual, util.UpCommand)
+      So(args.CommandParameter, ShouldEqual, 3)
 		})
 
-    Convey("When no arguments are available", func() {
+    Convey("no arguments must result in an error", func() {
       os.Args = []string{ "vmlcm" }
 
-      Convey("ParseArguments must return an error", func() {
-        args, err := util.ParseArguments()
-        So(err, ShouldNotBeNil)
-        So(args, ShouldBeNil)
-      })
+      args, err := util.ParseArguments()
+      So(err, ShouldNotBeNil)
+      So(args, ShouldBeNil)
     })
 	})
 }
