9c7fa37d5669f9020d21756c3653a534270d1767
Andre Kohn <andre@kohn.io>
2015-10-24 17:24:14 +0200

    Parsing config and flags in main()

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
-import fmt "fmt"
-import os "os"
-// import config "gitlab.kohn.io/ankoh/vmlcm/util"
+import(
+	"fmt"
+	"gitlab.kohn.io/ankoh/vmlcm/util"
+)
 
 func main() {
-	arguments := os.Args[1:]
-
-	if len(arguments) == 0 {
-		fmt.Println("You did not provide any arguments")
-	} else {
-		fmt.Println("Found the following arguments")
-		fmt.Println(arguments)
+	// First read arguments
+	args, argError := util.ParseArguments()
+	if argError != nil {
+		fmt.Println(argError.Error())
+		return
 	}
+
+	// Then read configuration
+	_, configError := util.ParseConfiguration(*args.ConfigPath)
+	if configError != nil {
+		fmt.Println(configError.Error())
+		return
+	}
+
+
 }
