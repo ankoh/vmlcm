5e6a6b0c19764533a9b35d34f30fe77ce768981d
Andre Kohn <andre@kohn.io>
2015-10-26 12:19:40 +0100

    Execute tests now also with goconvey

--- vmware/execute_test.go
+++ vmware/execute_test.go
@@ -2,26 +2,29 @@ package vmware_test
 
 import (
 	"testing"
-	"fmt"
+  . "github.com/smartystreets/goconvey/convey"
   "gitlab.kohn.io/ankoh/vmlcm/vmware"
 )
 
 func TestExecute(t *testing.T) {
-  // Should be the case...
+  Convey("Given a standard UNIX environment", t, func(context C) {
+    // Should be the case...
 
-  outChan := make(chan string)
-  errChan := make(chan string)
-  defer close(outChan)
-  defer close(errChan)
+    Convey("executeCommand must be able to execute 'ls -la ./'", func() {
+      outChan := make(chan string)
+      errChan := make(chan string)
+      defer close(outChan)
+      defer close(errChan)
 
-  // Dont call as goroutine as goconvey does not support it
-  go vmware.ExecuteCommand(outChan, errChan, "ls", "-la", "./")
+      // Dont call as goroutine as goconvey does not support it
+      go vmware.ExecuteCommand(outChan, errChan, "ls", "-la", "./")
 
-  select {
-    case out := <- outChan:
-      fmt.Printf(out)
-    case err := <- errChan:
-			fmt.Printf(err)
-      t.Fail()
-  }
+      select {
+        case out := <- outChan:
+        So(out, ShouldNotBeNil)
+				case err := <- errChan:
+				So(err, ShouldBeNil)
+      }
+    })
+  })
 }
