02dae98a902be96012eb33b35abc9cd0bd970b3a
Andre Kohn <andre@kohn.io>
2015-10-26 14:52:18 +0100

    Added VmrunWrapper

--- vmware/vmrun.go
+++ vmware/vmrun.go
@@ -3,29 +3,58 @@ package vmware
 import "fmt"
 import "sync"
 
-// Vmrun provides access to many of the VMware Fusion vmrun API
-type Vmrun struct {
+// VmrunWrapper provides access to the Vmrun binary
+type VmrunWrapper interface {
+  GetOutputChannel() chan string
+  GetErrorChannel() chan error
+
+  Start(vmx string)
+  Stop(vmx string, hard bool)
+  Reset(vmx string, hard bool)
+  Suspend(vmx string, hard bool)
+  Pause(vmx string)
+  Unpause(vmx string)
+  ListSnapshots(vmx string)
+  Snapshot(vmx string, name string)
+  DeleteSnapshot(vmx string)
+  RevertToSnapshot(vmx string, name string)
+  Delete(vmx string)
+  CloneLinked(template string, destination string, snapshot string, name string)
+}
+
+// CLIVmrun provides access to many of the VMware Fusion vmrun API through the command line
+type CLIVmrun struct {
   vmrunPath string
   vmrunMutex sync.Mutex
 
-  OutputChannel chan string
-  ErrorChannel chan error
+  outputChannel chan string
+  errorChannel chan error
+}
+
+// GetOutputChannel returns the outputchannel of the CLIVmrun wrapper
+func (vmrun *CLIVmrun) GetOutputChannel() chan string {
+  return vmrun.outputChannel
+}
+
+// GetErrorChannel returns the errorchannel of the CLIVmrun wrapper
+func (vmrun *CLIVmrun) GetErrorChannel() chan error {
+  return vmrun.errorChannel
 }
 
 // Start starts a VM
-func (vmrun *Vmrun) Start(vmx string) {
+func (vmrun *CLIVmrun) Start(vmx string) {
   vmrun.vmrunMutex.Lock()
   defer vmrun.vmrunMutex.Unlock()
 
   executeCommand(
-    vmrun.OutputChannel,
-    vmrun.ErrorChannel,
+    vmrun.outputChannel,
+    vmrun.errorChannel,
     vmrun.vmrunPath,
     "start", vmx, "nogui")
 }
 
 // Stop stops a VM
-func (vmrun *Vmrun) Stop(vmx string, hard bool) {
+func (vmrun *CLIVmrun) Stop(vmx string, hard bool) {
   vmrun.vmrunMutex.Lock()
   defer vmrun.vmrunMutex.Unlock()
 
@@ -37,14 +66,14 @@ func (vmrun *Vmrun) Stop(vmx string, hard bool) {
   }
 
   executeCommand(
-    vmrun.OutputChannel,
-    vmrun.ErrorChannel,
+    vmrun.outputChannel,
+    vmrun.errorChannel,
     vmrun.vmrunPath,
     "stop", vmx, force)
 }
 
 // Reset resets a VM
-func (vmrun *Vmrun) Reset(vmx string, hard bool) {
+func (vmrun *CLIVmrun) Reset(vmx string, hard bool) {
   vmrun.vmrunMutex.Lock()
   defer vmrun.vmrunMutex.Unlock()
 
@@ -56,14 +85,14 @@ func (vmrun *Vmrun) Reset(vmx string, hard bool) {
   }
 
   executeCommand(
-    vmrun.OutputChannel,
-    vmrun.ErrorChannel,
+    vmrun.outputChannel,
+    vmrun.errorChannel,
     vmrun.vmrunPath,
     "reset", vmx, force)
 }
 
 // Suspend suspends a VM
-func (vmrun *Vmrun) Suspend(vmx string, hard bool) {
+func (vmrun *CLIVmrun) Suspend(vmx string, hard bool) {
   vmrun.vmrunMutex.Lock()
   defer vmrun.vmrunMutex.Unlock()
 
@@ -75,104 +104,104 @@ func (vmrun *Vmrun) Suspend(vmx string, hard bool) {
   }
 
   executeCommand(
-    vmrun.OutputChannel,
-    vmrun.ErrorChannel,
+    vmrun.outputChannel,
+    vmrun.errorChannel,
     vmrun.vmrunPath,
     "suspend", vmx, force)
 }
 
 // Pause pauses a VM
-func (vmrun *Vmrun) Pause(vmx string) {
+func (vmrun *CLIVmrun) Pause(vmx string) {
   vmrun.vmrunMutex.Lock()
   defer vmrun.vmrunMutex.Unlock()
 
   executeCommand(
-    vmrun.OutputChannel,
-    vmrun.ErrorChannel,
+    vmrun.outputChannel,
+    vmrun.errorChannel,
     vmrun.vmrunPath,
     "pause", vmx)
 }
 
 // Unpause unpauses a VM
-func (vmrun *Vmrun) Unpause(vmx string) {
+func (vmrun *CLIVmrun) Unpause(vmx string) {
   vmrun.vmrunMutex.Lock()
   defer vmrun.vmrunMutex.Unlock()
 
   executeCommand(
-    vmrun.OutputChannel,
-    vmrun.ErrorChannel,
+    vmrun.outputChannel,
+    vmrun.errorChannel,
     vmrun.vmrunPath,
     "unpause", vmx)
 }
 
 // ListSnapshots lists the snapshots of a VM
-func (vmrun *Vmrun) ListSnapshots(vmx string) {
+func (vmrun *CLIVmrun) ListSnapshots(vmx string) {
   vmrun.vmrunMutex.Lock()
   defer vmrun.vmrunMutex.Unlock()
 
   executeCommand(
-    vmrun.OutputChannel,
-    vmrun.ErrorChannel,
+    vmrun.outputChannel,
+    vmrun.errorChannel,
     vmrun.vmrunPath,
     "listSnapshots", vmx)
 }
 
 // Snapshot creates a snapshot of a VM
-func (vmrun *Vmrun) Snapshot(vmx string, name string) {
+func (vmrun *CLIVmrun) Snapshot(vmx string, name string) {
   vmrun.vmrunMutex.Lock()
   defer vmrun.vmrunMutex.Unlock()
 
   executeCommand(
-    vmrun.OutputChannel,
-    vmrun.ErrorChannel,
+    vmrun.outputChannel,
+    vmrun.errorChannel,
     vmrun.vmrunPath,
     "snapshot", vmx, name)
 }
 
 // DeleteSnapshot deletes a snapshot of a VM
-func (vmrun *Vmrun) DeleteSnapshot(vmx string) {
+func (vmrun *CLIVmrun) DeleteSnapshot(vmx string) {
   vmrun.vmrunMutex.Lock()
   defer vmrun.vmrunMutex.Unlock()
 
   executeCommand(
-    vmrun.OutputChannel,
-    vmrun.ErrorChannel,
+    vmrun.outputChannel,
+    vmrun.errorChannel,
     vmrun.vmrunPath,
     "deleteSnapshot", vmx)
 }
 
 // RevertToSnapshot reverts a VM to a snapshot
-func (vmrun *Vmrun) RevertToSnapshot(vmx string, name string) {
+func (vmrun *CLIVmrun) RevertToSnapshot(vmx string, name string) {
   vmrun.vmrunMutex.Lock()
   defer vmrun.vmrunMutex.Unlock()
 
   executeCommand(
-    vmrun.OutputChannel,
-    vmrun.ErrorChannel,
+    vmrun.outputChannel,
+    vmrun.errorChannel,
     vmrun.vmrunPath,
     "revertToSnapshot", vmx, name)
 }
 
 // Delete deletes a VM
-func (vmrun *Vmrun) Delete(vmx string) {
+func (vmrun *CLIVmrun) Delete(vmx string) {
   vmrun.vmrunMutex.Lock()
   defer vmrun.vmrunMutex.Unlock()
 
   executeCommand(
-    vmrun.OutputChannel,
-    vmrun.ErrorChannel,
+    vmrun.outputChannel,
+    vmrun.errorChannel,
     vmrun.vmrunPath,
     "deleteVM", vmx)
 }
 
 // CloneLinked clones a linked VM
-func (vmrun *Vmrun) CloneLinked(template string, destination string, snapshot string, name string) {
+func (vmrun *CLIVmrun) CloneLinked(template string, destination string, snapshot string, name string) {
   vmrun.vmrunMutex.Lock()
   defer vmrun.vmrunMutex.Unlock()
 
   executeCommand(
-    vmrun.OutputChannel,
-    vmrun.ErrorChannel,
+    vmrun.outputChannel,
+    vmrun.errorChannel,
     vmrun.vmrunPath,
     "clone", template, destination,
     "linked",
