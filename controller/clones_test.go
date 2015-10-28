e19ae4c829d37f2a1a0b29b0338e5745fdb1d7a5
Andre Kohn <andre@kohn.io>
2015-10-28 09:49:49 +0100

    Started with clones test

--- controller/clones_test.go
+++ controller/clones_test.go
@@ -1,1 +1,37 @@
 package controller
+/*
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/ankoh/vmlcm/util"
+	"github.com/ankoh/vmlcm/vmware"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestVerification(t *testing.T) {
+	Convey("getAvailableMacAddresses should only return available MAC addresses", t, func() {
+
+	})
+}
+
+func createTestClonesFolders() {
+	os.Mkdir("/tmp/vmlcmclones", 0755)
+	os.Mkdir("/tmp/vmlcmclones/clones", 0755)
+}
+
+func createTestClonesTemplate() {
+	testBuffer := []byte("vmlcm test vmx\n")
+	ioutil.WriteFile("/tmp/vmlcmclones/test.vmx", testBuffer, 0644)
+}
+
+func createTestClonesVmrun() {
+	testBuffer := []byte("vmlcm test vmrun\n")
+	ioutil.WriteFile("/tmp/vmlcmclones/vmrun", testBuffer, 0755)
+}
+
+func deleteTestClonesFolders() {
+	os.RemoveAll("/tmp/vmlcmclones")
+}
+*/
